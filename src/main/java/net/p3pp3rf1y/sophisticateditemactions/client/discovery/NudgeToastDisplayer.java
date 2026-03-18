8162cbd6ac92dad0d661f298c66ffd37213e62e4
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-03-18 11:43:00 +0100

    refactor: ♻️ simplify nudge toast display path

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/client/discovery/NudgeToastDisplayer.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/client/discovery/NudgeToastDisplayer.java
@@ -2,11 +2,10 @@ package net.p3pp3rf1y.sophisticateditemactions.client.discovery;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.components.toasts.SystemToast;
+import net.minecraft.client.gui.components.toasts.ToastManager;
 import net.minecraft.network.chat.Component;
 import net.p3pp3rf1y.sophisticateditemactions.client.gui.ItemActionsTranslationHelper;
-
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 public class NudgeToastDisplayer {
 	public boolean showHint(NudgeHintType hintType, Component keybindName) {
@@ -21,17 +20,8 @@ public class NudgeToastDisplayer {
 			return true;
 		}
 
-		if (minecraft.gui != null) {
-			minecraft.gui.setOverlayMessage(description, false);
-			return true;
-		}
-
-		if (minecraft.player != null) {
-			minecraft.player.displayClientMessage(description, true);
-			return true;
-		}
-
-		return false;
+		minecraft.gui.setOverlayMessage(description, false);
+		return true;
 	}
 
 	private Component getTitle(NudgeHintType hintType) {
@@ -52,88 +42,8 @@ public class NudgeToastDisplayer {
 	}
 
 	private boolean tryShowSystemToast(Minecraft minecraft, Component title, Component description) {
-		try {
-			Object toastManager = getToastManager(minecraft);
-			if (toastManager == null) {
-				return false;
-			}
-
-			Class<?> systemToastClass = Class.forName("net.minecraft.client.gui.components.toasts.SystemToast");
-			Object toastId = getToastId(systemToastClass);
-			if (toastId == null) {
-				return false;
-			}
-
-			Object toast = createSystemToast(systemToastClass, toastManager, toastId, title, description);
-			if (toast == null) {
-				return false;
-			}
-
-			return addToast(toastManager, toast);
-		} catch (Exception ignored) {
-			return false;
-		}
-	}
-
-	private Object getToastManager(Minecraft minecraft) {
-		try {
-			return Minecraft.class.getMethod("getToasts").invoke(minecraft);
-		} catch (Exception ignored) {
-			try {
-				return Minecraft.class.getMethod("getToastManager").invoke(minecraft);
-			} catch (Exception ignoredAgain) {
-				return null;
-			}
-		}
-	}
-
-	private Object getToastId(Class<?> systemToastClass) {
-		for (Class<?> declaredClass : systemToastClass.getDeclaredClasses()) {
-			if (!declaredClass.isEnum()) {
-				continue;
-			}
-			Object[] constants = declaredClass.getEnumConstants();
-			if (constants == null || constants.length == 0) {
-				continue;
-			}
-			for (Object constant : constants) {
-				if (constant.toString().contains("TUTORIAL") || constant.toString().contains("PERIODIC")) {
-					return constant;
-				}
-			}
-			return constants[0];
-		}
-		return null;
-	}
-
-	private Object createSystemToast(Class<?> systemToastClass, Object toastManager, Object toastId, Component title, Component description) throws ReflectiveOperationException {
-		for (Method method : systemToastClass.getMethods()) {
-			if (!Modifier.isStatic(method.getModifiers()) || method.getReturnType() != systemToastClass) {
-				continue;
-			}
-			Class<?>[] parameterTypes = method.getParameterTypes();
-			if (parameterTypes.length == 4
-					&& parameterTypes[0].isAssignableFrom(toastManager.getClass())
-					&& parameterTypes[1].isAssignableFrom(toastId.getClass())
-					&& parameterTypes[2] == Component.class
-					&& parameterTypes[3] == Component.class) {
-				return method.invoke(null, toastManager, toastId, title, description);
-			}
-		}
-		return null;
-	}
-
-	private boolean addToast(Object toastManager, Object toast) throws ReflectiveOperationException {
-		for (Method method : toastManager.getClass().getMethods()) {
-			if (method.getParameterCount() != 1) {
-				continue;
-			}
-			if (method.getName().equals("addToast") || method.getName().equals("add")) {
-				method.invoke(toastManager, toast);
-				return true;
-			}
-		}
-
-		return false;
+		ToastManager toastManager = minecraft.getToastManager();
+		SystemToast.add(toastManager, SystemToast.SystemToastId.PERIODIC_NOTIFICATION, title, description);
+		return true;
 	}
 }
