f4a217d0b3cc097e8551e55599b0a54e1e02cd20
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-16 20:54:35 +0200

    fix: 🐛 Discovery nudges no longer trigger on backpack and settings screens

---
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/client/NudgeScreenSuppressorRegistry.java
@@ -0,0 +1,27 @@
+package net.p3pp3rf1y.sophisticateditemactions.client;
+
+import net.minecraft.client.gui.screens.Screen;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class NudgeScreenSuppressorRegistry {
+	private static final List<Predicate<Screen>> SUPPRESSORS = new ArrayList<>();
+
+	private NudgeScreenSuppressorRegistry() {}
+
+	public static void register(Predicate<Screen> suppressor) {
+		SUPPRESSORS.add(suppressor);
+	}
+
+	public static boolean isSuppressed(Screen screen) {
+		for (Predicate<Screen> suppressor : SUPPRESSORS) {
+			if (suppressor.test(screen)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
