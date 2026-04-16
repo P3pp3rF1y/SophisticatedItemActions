c953600d5c755951a66f0d6e44ad6df2ee6679ae
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-16 20:54:35 +0200

    fix: 🐛 Discovery nudges no longer trigger on backpack and settings screens

---
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/sophisticatedbackpacks/SophisticatedBackpacksCompat.java
@@ -0,0 +1,20 @@
+package net.p3pp3rf1y.sophisticateditemactions.compat.sophisticatedbackpacks;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+
+public class SophisticatedBackpacksCompat implements ICompat {
+	@Override
+	public void init(IEventBus modBus) {
+		if (FMLEnvironment.getDist() == Dist.CLIENT) {
+			SophisticatedBackpacksClientCompat.init();
+		}
+	}
+
+	@Override
+	public void setup() {
+		//noop
+	}
+}
