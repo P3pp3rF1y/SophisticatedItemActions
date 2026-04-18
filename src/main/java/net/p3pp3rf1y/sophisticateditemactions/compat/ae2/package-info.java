af24755da22e8f9606efa5b811e32192d371c1d9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-18 13:23:59 +0200

    chore: 🔧 Added item action compat for AE2 terminals and ME chest

---
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/ae2/package-info.java
@@ -0,0 +1,8 @@
+// Auto generated package-info by MCP
+
+@ParametersAreNonnullByDefault @NullMarked
+package net.p3pp3rf1y.sophisticateditemactions.compat.ae2;
+
+import org.jspecify.annotations.NullMarked;
+
+import javax.annotation.ParametersAreNonnullByDefault;
