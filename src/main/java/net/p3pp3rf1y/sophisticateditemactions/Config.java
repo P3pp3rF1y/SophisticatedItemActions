68dbfd0d94c840f6db93139739e88f9db6790653
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-19 21:10:10 +0200

    feat: ✨ Added in-game NeoForge config GUI support and labels

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/Config.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/Config.java
@@ -1,6 +1,7 @@
 package net.p3pp3rf1y.sophisticateditemactions;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
+import net.p3pp3rf1y.sophisticateditemactions.client.gui.ItemActionsTranslationHelper;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class Config {
@@ -20,11 +21,7 @@ public class Config {
 		public final DiscoveryNudges discoveryNudges;
 
 		public Client(ModConfigSpec.Builder builder) {
-			builder.comment("Client-side Settings").push("client");
-
 			discoveryNudges = new DiscoveryNudges(builder);
-
-			builder.pop();
 		}
 	}
 
@@ -42,19 +39,21 @@ public class Config {
 		public final ModConfigSpec.BooleanValue debugLogging;
 
 		public DiscoveryNudges(ModConfigSpec.Builder builder) {
-			builder.comment("Client-only discoverability nudges for highlight, restock and deposit keybind actions").push("discoveryNudges");
+			builder.comment("Client-only discoverability nudges for highlight, restock and deposit keybind actions")
+					.translation(ItemActionsTranslationHelper.INSTANCE.translConfig("discoveryNudges"))
+					.push("discoveryNudges");
 
-			enabled = builder.comment("Master toggle for discovery nudges").define("enabled", true);
-			globalCooldownTicks = builder.comment("Global cooldown between any two nudges in ticks (default: 2 hours)").defineInRange("globalCooldownTicks", 144000, 0, Integer.MAX_VALUE);
-			maxNudgesPerSession = builder.comment("Maximum total number of nudges shown during one world connection session").defineInRange("maxNudgesPerSession", 3, 0, Integer.MAX_VALUE);
-			highlightActionThreshold = builder.comment("Number of storage opens without transfers before showing highlight hint (-1 disables)").defineInRange("highlightActionThreshold", 4, -1, Integer.MAX_VALUE);
-			restockActionThreshold = builder.comment("Number of storage sessions with transfer from storage to inventory before showing restock hint (-1 disables)").defineInRange("restockActionThreshold", 3, -1, Integer.MAX_VALUE);
-			depositActionThreshold = builder.comment("Number of storage sessions with transfer from inventory to storage before showing deposit hint (-1 disables)").defineInRange("depositActionThreshold", 3, -1, Integer.MAX_VALUE);
-			highlightEnabled = builder.comment("Whether highlight hint can be shown").define("highlightEnabled", true);
-			restockEnabled = builder.comment("Whether restock hint can be shown").define("restockEnabled", true);
-			depositEnabled = builder.comment("Whether deposit hint can be shown").define("depositEnabled", true);
-			checkIntervalTicks = builder.comment("How often to evaluate nudge state in ticks").defineInRange("checkIntervalTicks", 40, 1, Integer.MAX_VALUE);
-			debugLogging = builder.comment("Logs nudge state transitions to debug log").define("debugLogging", false);
+			enabled = builder.comment("Master toggle for discovery nudges").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("enabled")).define("enabled", true);
+			globalCooldownTicks = builder.comment("Global cooldown between any two nudges in ticks (default: 2 hours)").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("globalCooldownTicks")).defineInRange("globalCooldownTicks", 144000, 0, Integer.MAX_VALUE);
+			maxNudgesPerSession = builder.comment("Maximum total number of nudges shown during one world connection session").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("maxNudgesPerSession")).defineInRange("maxNudgesPerSession", 3, 0, Integer.MAX_VALUE);
+			highlightActionThreshold = builder.comment("Number of storage opens without transfers before showing highlight hint (-1 disables)").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("highlightActionThreshold")).defineInRange("highlightActionThreshold", 4, -1, Integer.MAX_VALUE);
+			restockActionThreshold = builder.comment("Number of storage sessions with transfer from storage to inventory before showing restock hint (-1 disables)").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("restockActionThreshold")).defineInRange("restockActionThreshold", 3, -1, Integer.MAX_VALUE);
+			depositActionThreshold = builder.comment("Number of storage sessions with transfer from inventory to storage before showing deposit hint (-1 disables)").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("depositActionThreshold")).defineInRange("depositActionThreshold", 3, -1, Integer.MAX_VALUE);
+			highlightEnabled = builder.comment("Whether highlight hint can be shown").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("highlightEnabled")).define("highlightEnabled", true);
+			restockEnabled = builder.comment("Whether restock hint can be shown").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("restockEnabled")).define("restockEnabled", true);
+			depositEnabled = builder.comment("Whether deposit hint can be shown").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("depositEnabled")).define("depositEnabled", true);
+			checkIntervalTicks = builder.comment("How often to evaluate nudge state in ticks").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("checkIntervalTicks")).defineInRange("checkIntervalTicks", 40, 1, Integer.MAX_VALUE);
+			debugLogging = builder.comment("Logs nudge state transitions to debug log").translation(ItemActionsTranslationHelper.INSTANCE.translConfig("debugLogging")).define("debugLogging", false);
 
 			builder.pop();
 		}
--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/SophisticatedItemActions.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/SophisticatedItemActions.java
@@ -4,8 +4,11 @@ import net.minecraft.resources.Identifier;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.p3pp3rf1y.sophisticateditemactions.client.ClientEventHandler;
 import net.p3pp3rf1y.sophisticateditemactions.common.CommonEventHandler;
 import net.p3pp3rf1y.sophisticateditemactions.init.ModCompat;
@@ -19,6 +22,9 @@ public class SophisticatedItemActions {
 
 	public SophisticatedItemActions(IEventBus modBus, Dist dist, ModContainer container) {
 		container.registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_SPEC);
+		if (dist == Dist.CLIENT && !ModList.get().isLoaded("configured")) {
+			container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+		}
 		CommonEventHandler.registerHandlers(modBus);
 		ModCompat.register();
 		if (dist == Dist.CLIENT) {
