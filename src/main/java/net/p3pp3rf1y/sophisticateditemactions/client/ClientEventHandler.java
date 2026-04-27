3882a743a0f8468d9229b5b4614263362554a4b0
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-27 08:41:29 +0200

    chore: 🔧 port Sable sublevel storage support and highlights

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/client/ClientEventHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/client/ClientEventHandler.java
@@ -24,7 +24,7 @@ import net.p3pp3rf1y.sophisticateditemactions.client.gui.HighlightDirectionOverl
 import net.p3pp3rf1y.sophisticateditemactions.client.gui.ItemActionsTranslationHelper;
 import net.p3pp3rf1y.sophisticateditemactions.client.render.EntityHighlightRenderer;
 import net.p3pp3rf1y.sophisticateditemactions.client.render.ItemFlightAnimator;
-import net.p3pp3rf1y.sophisticateditemactions.client.render.RenderedEntityBlockHighlightRenderer;
+import net.p3pp3rf1y.sophisticateditemactions.client.render.RenderedBlockHighlightRenderer;
 import net.p3pp3rf1y.sophisticateditemactions.common.HighlightHandler;
 import net.p3pp3rf1y.sophisticateditemactions.common.ItemTransferHandler;
 import org.lwjgl.glfw.GLFW;
@@ -98,7 +98,8 @@ public class ClientEventHandler {
 	private static void renderLevelStage(RenderLevelStageEvent.AfterBlockEntities event) {
 		float partialTick = event.getPartialTick().getGameTimeDeltaPartialTick(false);
 		ItemFlightAnimator.render(event.getPoseStack(), partialTick, event.getCamera().getPosition());
-		RenderedEntityBlockHighlightRenderer.render(event.getPoseStack(), partialTick, event.getCamera().getPosition());
+		RenderedBlockHighlightRenderer.render(event.getPoseStack(), partialTick, event.getCamera().getPosition());
+		ClientCompatRenderHelper.renderLevelStage(event.getPoseStack(), partialTick, event.getCamera().getPosition());
 		EntityHighlightRenderer.render(event.getPoseStack(), partialTick, event.getCamera().getPosition());
 	}
 
--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/init/ModCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/init/ModCompat.java
@@ -1,6 +1,11 @@
 package net.p3pp3rf1y.sophisticateditemactions.init;
 
 import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.CREATE;
+import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.REFINED_STORAGE;
+import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.SABLE;
+import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.SOPHISTICATED_BACKPACKS;
+import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.STORAGE_IN_MOTION;
+
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatInfo;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatModIds;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatRegistry;
@@ -12,10 +17,7 @@ import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.jei.JeiCompat
 import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.rei.ReiCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.sophisticatedbackpacks.SophisticatedBackpacksCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.sophisticatedstorageinmotion.StorageInMotionCompat;
-
-import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.REFINED_STORAGE;
-import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.SOPHISTICATED_BACKPACKS;
-import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.STORAGE_IN_MOTION;
+import net.p3pp3rf1y.sophisticateditemactions.compat.sable.SableCompat;
 
 public class ModCompat {
 	private ModCompat() {}
@@ -27,6 +29,7 @@ public class ModCompat {
 		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.EMI), () -> modBus -> new EmiCompat());
 		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.REI), () -> modBus -> new ReiCompat());
 		CompatRegistry.registerCompat(new CompatInfo(REFINED_STORAGE), () -> modBus -> new RefinedStorageCompat());
+		CompatRegistry.registerCompat(new CompatInfo(SABLE), () -> modBus -> new SableCompat());
 		CompatRegistry.registerCompat(new CompatInfo(SOPHISTICATED_BACKPACKS), () -> modBus -> new SophisticatedBackpacksCompat());
 		CompatRegistry.registerCompat(new CompatInfo(STORAGE_IN_MOTION), () -> modBus -> new StorageInMotionCompat());
 	}
