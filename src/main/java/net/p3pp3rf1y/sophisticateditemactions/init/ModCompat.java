ea0419b1e00f91509af62ea48e87ee86c0be77f4
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-05-05 08:35:08 +0200

    fix: 🐛 Fixed Sophisticated Storage Storage I/O, Output and Input to not get highlighted during item search

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/init/ModCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/init/ModCompat.java
@@ -4,6 +4,7 @@ import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.CREATE;
 import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.REFINED_STORAGE;
 import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.SABLE;
 import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.SOPHISTICATED_BACKPACKS;
+import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.SOPHISTICATED_STORAGE;
 import static net.p3pp3rf1y.sophisticateditemactions.compat.CompatModIds.STORAGE_IN_MOTION;
 
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatInfo;
@@ -16,6 +17,7 @@ import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.emi.EmiCompat
 import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.jei.JeiCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.recipeviewers.rei.ReiCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.sophisticatedbackpacks.SophisticatedBackpacksCompat;
+import net.p3pp3rf1y.sophisticateditemactions.compat.sophisticatedstorage.SophisticatedStorageCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.sophisticatedstorageinmotion.StorageInMotionCompat;
 import net.p3pp3rf1y.sophisticateditemactions.compat.sable.SableCompat;
 
@@ -31,6 +33,7 @@ public class ModCompat {
 		CompatRegistry.registerCompat(new CompatInfo(REFINED_STORAGE), () -> modBus -> new RefinedStorageCompat());
 		CompatRegistry.registerCompat(new CompatInfo(SABLE), () -> modBus -> new SableCompat());
 		CompatRegistry.registerCompat(new CompatInfo(SOPHISTICATED_BACKPACKS), () -> modBus -> new SophisticatedBackpacksCompat());
+		CompatRegistry.registerCompat(new CompatInfo(SOPHISTICATED_STORAGE), () -> modBus -> new SophisticatedStorageCompat());
 		CompatRegistry.registerCompat(new CompatInfo(STORAGE_IN_MOTION), () -> modBus -> new StorageInMotionCompat());
 	}
 }
