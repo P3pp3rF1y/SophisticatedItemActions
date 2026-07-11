62f6224790f83ed9a28553164c20c4c053a39c3c
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-11 14:27:36 +0200

    chore: 🔧 Forward port FTB Library compat update

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/ftblibrary/FtbLibraryClientCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/ftblibrary/FtbLibraryClientCompat.java
@@ -1,9 +1,9 @@
 package net.p3pp3rf1y.sophisticateditemactions.compat.ftblibrary;
 
-import dev.ftb.mods.ftblibrary.ui.IFocusableWidget;
-import dev.ftb.mods.ftblibrary.ui.Panel;
-import dev.ftb.mods.ftblibrary.ui.ScreenWrapper;
-import dev.ftb.mods.ftblibrary.ui.Widget;
+import dev.ftb.mods.ftblibrary.client.gui.widget.IFocusableWidget;
+import dev.ftb.mods.ftblibrary.client.gui.widget.Panel;
+import dev.ftb.mods.ftblibrary.client.gui.widget.ScreenWrapper;
+import dev.ftb.mods.ftblibrary.client.gui.widget.Widget;
 import net.minecraft.client.gui.screens.Screen;
 import net.p3pp3rf1y.sophisticateditemactions.client.ClientEventHandler;
 
