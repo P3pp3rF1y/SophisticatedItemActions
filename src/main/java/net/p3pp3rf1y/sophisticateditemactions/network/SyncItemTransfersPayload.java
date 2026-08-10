3d3af88a588ea328a8d9b6d22790f8fa5fe03538
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-10 11:52:14 +0200

    chore: ♻️ Forward ported recipe restocking support for EMI and REI

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/network/SyncItemTransfersPayload.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/network/SyncItemTransfersPayload.java
@@ -16,16 +16,19 @@ import net.p3pp3rf1y.sophisticatedcore.SophisticatedCore;
 import net.p3pp3rf1y.sophisticatedcore.util.RandHelper;
 import net.p3pp3rf1y.sophisticatedcore.util.StreamCodecHelper;
 import net.p3pp3rf1y.sophisticateditemactions.client.ChestOpeningAnimator;
+import net.p3pp3rf1y.sophisticateditemactions.client.ClientEventHandler;
 import net.p3pp3rf1y.sophisticateditemactions.client.render.ItemFlightAnimator;
 import net.p3pp3rf1y.sophisticateditemactions.common.ItemTransferData;
 
 import java.util.List;
 
-public record SyncItemTransfersPayload(List<ItemTransferData> itemTransferData, Vec3 playerPos, boolean fromPlayer) implements CustomPacketPayload {
+public record SyncItemTransfersPayload(List<ItemTransferData> itemTransferData, Vec3 playerPos, boolean fromPlayer,
+		boolean recipeRestock) implements CustomPacketPayload {
 	public static final Type<SyncItemTransfersPayload> TYPE = new Type<>(SophisticatedCore.getRL("sync_item_transfers"));
 	public static final StreamCodec<RegistryFriendlyByteBuf, SyncItemTransfersPayload> STREAM_CODEC = StreamCodec.composite(
 			ItemTransferData.STREAM_CODEC.apply(ByteBufCodecs.list()), SyncItemTransfersPayload::itemTransferData, StreamCodecHelper.VEC3,
-			SyncItemTransfersPayload::playerPos, ByteBufCodecs.BOOL, SyncItemTransfersPayload::fromPlayer, SyncItemTransfersPayload::new);
+			SyncItemTransfersPayload::playerPos, ByteBufCodecs.BOOL, SyncItemTransfersPayload::fromPlayer, ByteBufCodecs.BOOL,
+			SyncItemTransfersPayload::recipeRestock, SyncItemTransfersPayload::new);
 
 	@Override
 	public Type<? extends CustomPacketPayload> type() {
@@ -49,5 +52,8 @@ public record SyncItemTransfersPayload(List<ItemTransferData> itemTransferData, 
 					: RandHelper.getRandomMinusOneToOne(level.random) * 1.4F + 2.0F;
 			level.playSound(player, to.x(), to.y(), to.z(), SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, 0.3F, pitch);
 		});
+		if (payload.recipeRestock()) {
+			ClientEventHandler.handleRecipeRestockSync();
+		}
 	}
 }
