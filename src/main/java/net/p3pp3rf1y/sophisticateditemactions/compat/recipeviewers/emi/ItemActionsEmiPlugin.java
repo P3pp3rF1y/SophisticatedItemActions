7a56481f1cb32465b080e7f610ba5e073d4c9a8a
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-10 14:42:42 +0200

    chore: ♻️ Optimize forward ported recipe restocking support for EMI and REI

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/recipeviewers/emi/ItemActionsEmiPlugin.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/compat/recipeviewers/emi/ItemActionsEmiPlugin.java
@@ -33,8 +33,8 @@ public class ItemActionsEmiPlugin implements EmiPlugin {
 	}
 
 	private static void addRestockButton(EmiRecipe recipe, WidgetHolder widgets) {
-		if (recipe.getCategory() != VanillaEmiRecipeCategories.CRAFTING || !Config.SERVER.recipeRestockEnabled.get() || Minecraft.getInstance().player == null
-				|| getIngredientOptions(recipe, false).isEmpty()) {
+		if (recipe.getCategory() != VanillaEmiRecipeCategories.CRAFTING || !Config.SERVER.recipeRestockEnabled.get()
+				|| Minecraft.getInstance().player == null) {
 			return;
 		}
 		int x = widgets.getWidth() + 5;
