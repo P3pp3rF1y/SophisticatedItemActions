1e95326e3dffeaf94e42ad284c57f846b61a9b64
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-22 13:44:53 +0200

    chore: ♻️ Applied formatting to registered recipe restock payload

--- src/main/java/net/p3pp3rf1y/sophisticateditemactions/network/RestockRegisteredRecipePayload.java
+++ src/main/java/net/p3pp3rf1y/sophisticateditemactions/network/RestockRegisteredRecipePayload.java
@@ -21,10 +21,9 @@ public record RestockRegisteredRecipePayload(Identifier recipeId, boolean fullSt
 	public static final Type<RestockRegisteredRecipePayload> TYPE = new Type<>(SophisticatedCore.getIdentifier("restock_registered_recipe"));
 	public static final StreamCodec<RegistryFriendlyByteBuf, RestockRegisteredRecipePayload> STREAM_CODEC = StreamCodec.composite(Identifier.STREAM_CODEC,
 			RestockRegisteredRecipePayload::recipeId, ByteBufCodecs.BOOL, RestockRegisteredRecipePayload::fullStacks,
-			ByteBufCodecs.map(HashMap::new, Identifier.STREAM_CODEC, BlockPos.STREAM_CODEC.apply(ByteBufCodecs.list(MAX_TARGETS_PER_GROUP)),
-					MAX_TARGET_GROUPS),
-			RestockRegisteredRecipePayload::storagePositions, ByteBufCodecs.map(HashMap::new, Identifier.STREAM_CODEC,
-					ByteBufCodecs.INT.apply(ByteBufCodecs.list(MAX_TARGETS_PER_GROUP)), MAX_TARGET_GROUPS),
+			ByteBufCodecs.map(HashMap::new, Identifier.STREAM_CODEC, BlockPos.STREAM_CODEC.apply(ByteBufCodecs.list(MAX_TARGETS_PER_GROUP)), MAX_TARGET_GROUPS),
+			RestockRegisteredRecipePayload::storagePositions,
+			ByteBufCodecs.map(HashMap::new, Identifier.STREAM_CODEC, ByteBufCodecs.INT.apply(ByteBufCodecs.list(MAX_TARGETS_PER_GROUP)), MAX_TARGET_GROUPS),
 			RestockRegisteredRecipePayload::entityIds, RestockRegisteredRecipePayload::new);
 
 	@Override
